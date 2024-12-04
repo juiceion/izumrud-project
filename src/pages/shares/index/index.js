a7cd99e636ed34678634d23ed91e7b5ef46d6f9e
Ilya Okruzhko <i.okruzhko@think24.ru>
2024-12-04 15:35:36 +0700

    add shares page

---
+++ src/pages/shares/index/index.js
@@ -0,0 +1,6 @@
+import 'modules/layout/footer';
+import 'modules/layout/header';
+import 'modules/layout/layout';
+import 'modules/vacancy/vacancy-card'
+import 'modules/common/svg-icon';
+import './style.less';

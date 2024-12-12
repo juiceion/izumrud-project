f0400b2311326f52ee75d3c62917ed52cbbe33ce
nikitosalo <nssalikov@gmail.com>
2024-12-12 16:58:35 +0300

    feat: начал делать страницу магазина

---
+++ src/pages/shop/index/index.js
@@ -0,0 +1,8 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'modules/layout/footer';
+import 'modules/layout/header';
+import 'modules/layout/layout';
+import 'modules/common/promo-card';
+import './style.less';

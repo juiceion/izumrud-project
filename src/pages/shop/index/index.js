871d8777cc06b4739dc7b4a93d46388f9f469498
nikitosalo <nssalikov@gmail.com>
2024-12-15 19:07:34 +0300

    feat: добавил с траницу магазина

--- src/pages/shop/index/index.js
+++ src/pages/shop/index/index.js
@@ -6,3 +6,95 @@ import 'modules/layout/header';
 import 'modules/layout/layout';
 import 'modules/common/promo-card';
 import './style.less';
+import Swiper from "swiper";
+import {Autoplay, FreeMode} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+class SliderElement {
+    constructor(el) {
+        this.el = el;
+
+        this.observer = new ResizeObserver((entries) => {
+            const target = entries[0];
+            const width = target.contentRect.width;
+
+            swiper2.params.slidesOffsetBefore = width / 2;
+            swiper2.update();
+        });
+
+        this.observer.observe(this.el);
+    }
+}
+
+const node = document.querySelector('.first-slider-item');
+
+if (node) {
+    new SliderElement(node);
+}
+
+const swiper1 = new Swiper('.first-slider', {
+        modules: [Autoplay, FreeMode],
+        freeMode: true,
+        loop: true,
+        allowTouchMove: false,
+        slidesPerView: 2,
+        spaceBetween: 15,
+        autoplay: {
+            enabled: true,
+            delay: 0,
+        },
+        speed: 4000,
+        breakpoints: {
+            640: {
+                slidesPerView: 3,
+                spaceBetween: 15,
+            },
+            960: {
+                slidesPerView: 4,
+            }
+        }
+    }
+);
+
+const swiper2 = new Swiper('.second-slider', {
+    modules: [Autoplay, FreeMode],
+    freeMode: true,
+    loop: true,
+    allowTouchMove: false,
+    slidesPerView: 2,
+    spaceBetween: 15,
+    slidesOffsetBefore: node ? node.clientWidth / 2 : 0,
+    autoplay: {
+        enabled: true,
+        delay: 0,
+    },
+    speed: 4000,
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        960: {
+            slidesPerView: 4,
+        }
+    }
+    }
+);
+
+const imageItem = document.querySelector('.page-shop__desktop-photos-container__img');
+const container = document.querySelector('.page-shop__desktop-photos-container');
+if (imageItem && container) {
+    container.style.setProperty('--height', `${imageItem.clientWidth * 2 + 50}px`);
+
+    const observer = new ResizeObserver((entries) => {
+        const target = entries[0];
+        const height = target.contentRect.height;
+
+        container.style.setProperty('--height', `${height * 2 + 30}px`);
+    });
+
+    observer.observe(imageItem);
+}
+

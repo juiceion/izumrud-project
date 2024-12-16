0a0724418f87a7f73745f68a2122a42e6876ec13
nikitosalo <nssalikov@gmail.com>
2024-12-16 16:04:44 +0300

    feat: добавил страницу каталога

--- src/pages/shop/index/index.js
+++ src/pages/shop/index/index.js
@@ -1,53 +1,28 @@
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import 'modules/layout/footer';
 import 'modules/layout/header';
 import 'modules/layout/layout';
 import 'modules/common/promo-card';
 import './style.less';
-import Swiper from "swiper";
-import {Autoplay, FreeMode} from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
-const swiper1 = new Swiper('.first-slider', {
-        modules: [Autoplay, FreeMode],
-        freeMode: true,
-        loop: true,
-        allowTouchMove: false,
-        slidesPerView: 2,
-        spaceBetween: 15,
-        autoplay: {
-            enabled: true,
-            delay: 0,
-        },
-        speed: 4000,
-        breakpoints: {
-            640: {
-                slidesPerView: 3,
-                spaceBetween: 15,
-            },
-            960: {
-                slidesPerView: 4,
-            }
-        }
+class catalogItem {
+    constructor(elem) {
+        this.elem = elem;
+
+        this.elem.children[0].addEventListener('click', () => {
+            this.elem.classList.toggle('active');
+        })
     }
-);
-
-const imageItem = document.querySelector('.page-shop__desktop-photos-container__img');
-const container = document.querySelector('.page-shop__desktop-photos-container');
-if (imageItem && container) {
-    container.style.setProperty('--height', `${imageItem.clientWidth * 2 + 50}px`);
-
-    const observer = new ResizeObserver((entries) => {
-        const target = entries[0];
-        const height = target.contentRect.height;
-
-        container.style.setProperty('--height', `${height * 2 + 30}px`);
-    });
-
-    observer.observe(imageItem);
 }
+
+const mobileNodes = document.querySelectorAll('.page-catalog__mobile-item');
+
+mobileNodes.forEach((node) => {
+    new catalogItem(node);
+})
+
+const desktopNodes = document.querySelectorAll('.page-catalog__desktop-menu-item');
+
+desktopNodes.forEach((node) => {
+    new catalogItem(node);
+})
 
